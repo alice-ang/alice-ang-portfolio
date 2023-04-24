6505b2a2955e6eb1d873fa73e94e459e2f9d25fa
Alice A <alice.anglesjo@hotmail.com>
2023-04-24 22:42:48 +0200

    fix: meta document error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,6 @@ export default function Document() {
   return (
     <Html lang='en' suppressHydrationWarning>
       <Head>
-        <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0'
-        />
-
         <link
           rel='preload'
           href='/fonts/inter-var-latin.woff2'
